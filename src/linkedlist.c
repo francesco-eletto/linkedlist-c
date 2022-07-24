b6afab273b234309258ccdf3e25c7b17b3885463
Francesco Eletto <109898985+francesco-eletto@users.noreply.github.com>
2022-07-24 14:13:19 +0200

    wip: library header

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,2 +1,18 @@
+#include <malloc.h>
 #include "linkedlist.h"
 
+
+LinkedList* ll_new() {
+    // TODO body
+    
+}
+
+void ll_add(LinkedList *list, int value) {
+    // TODO body
+    
+}
+
+void ll_del(LinkedList *list) {
+    // TODO body
+    
+}
--- src/linkedlist.h
+++ src/linkedlist.h
@@ -1,6 +1,16 @@
 #ifndef __LINKEDLIST__
 #define __LINKEDLIST__
 
+typedef struct {
+    int value;
+    LinkedList *next;
+} LinkedList;
 
+
+LinkedList* ll_new();
+
+void ll_add(LinkedList *list, int value);
+
+void ll_del(LinkedList *list);
 
 #endif
